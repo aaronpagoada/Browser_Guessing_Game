0c5c29152558aa6691701d99e3927a1e0a3699ed
Aaron Pagoada <aaron.pagoada@gmail.com>
2021-01-07 17:30:31 -0500

    script written

---
+++ numberGuessingGame/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let secretNum = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = (message) => {
+    document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        displayMessage('Enter a number!');
+    }else if(guess === secretNum){
+        displayMessage('Correct!');
+        document.querySelector('.number').textContent = secretNum;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    }else if(guess !== secretNum){
+        if(score > 1){
+            displayMessage(guess > secretNum ? 'Too high!' : 'Too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else{
+            displayMessage('You lost!');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
