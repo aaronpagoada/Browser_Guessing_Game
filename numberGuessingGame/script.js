96f7b808fffd599327e22de463382f4c3546ec10
Aaron Pagoada <aaron.pagoada@gmail.com>
2021-01-07 17:36:03 -0500

    Finished script

--- numberGuessingGame/script.js
+++ numberGuessingGame/script.js
@@ -37,3 +37,16 @@ document.querySelector('.check').addEventListener('click', () => {
     }
 });
 
+document.querySelector('.again').addEventListener('click', () => {
+    score = 20;
+    secretNum = Math.trunc(Math.random() * 20) + 1;
+
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
+
